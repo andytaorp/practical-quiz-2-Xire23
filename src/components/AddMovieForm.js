cb8f56270ff32cba24214e51e3040dcf7768689e
Xire23 <23025337@myrp.edu.sg>
2024-12-03 14:34:25 +0800

    r

--- src/components/AddMovieForm.js
+++ src/components/AddMovieForm.js
@@ -5,7 +5,30 @@ export default function AddMovieForm({ onAddMovie }) {
 
   const handleSubmit = (e) => {
     // TODO: write code to handle form submission
+    e.preventDefault();
+    if (!title) return;
+
+    const newTitle = {
+      id: Date.now(),
+      name: title,
+      completed: false,
+    };
+
+    onAddMovie(newTitle);
+    setTitle("");
   };
 
-  return; // TODO: add a form to add a new movie
+  return(
+    // TODO: add a form to add a new movie
+    <form onSubmit = { handleSubmit } >
+        <input
+          type="text"
+          placeholder="New Movie"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)
+          }
+        />
+        <button>Add Movie</button> 
+      </form >
+  ) 
 }
