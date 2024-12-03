b436794f6b30345da0e23cd7f7b5082824038af7
Xire23 <23025337@myrp.edu.sg>
2024-12-03 14:44:56 +0800

    r

--- src/components/AddMovieForm.js
+++ src/components/AddMovieForm.js
@@ -4,31 +4,29 @@ export default function AddMovieForm({ onAddMovie }) {
   const [title, setTitle] = useState("");
 
   const handleSubmit = (e) => {
-    // TODO: write code to handle form submission
     e.preventDefault();
-    if (!title) return;
+    
+    if (!title.trim()) return;
 
-    const newTitle = {
+    const newMovie = {
       id: Date.now(),
       name: title,
       completed: false,
     };
 
-    onAddMovie(newTitle);
+    onAddMovie(newMovie);
     setTitle("");
   };
 
-  return(
-    // TODO: add a form to add a new movie
-    <form onSubmit = { handleSubmit } >
-        <input
-          type="text"
-          placeholder="New Movie"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)
-          }
-        />
-        <button>Add Movie</button> 
-      </form >
-  ) 
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Movie Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button type="submit">Add Movie</button>
+    </form>
+  );
 }
